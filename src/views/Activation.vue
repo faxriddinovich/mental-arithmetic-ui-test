29a9ad46996fad76397b98cc0070b503ebc8c7d8
mhw0 <mhw0@yahoo.com>
2021-06-10 22:32:17 +0500

    update the activation page

--- src/views/Activation.vue
+++ src/views/Activation.vue
@@ -3,25 +3,27 @@
     <div
       class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center"
     >
-      <b-upload drag-drop expanded>
-        <section class="section">
-          <div class="content has-text-centered">
-            <p>
-              <b-icon icon="upload" size="is-large"> </b-icon>
-            </p>
-            <p>Drop your certificate file here or click to upload</p>
-          </div>
-        </section>
-      </b-upload>
-      <b-button
-        type="is-primary"
-        tag="router-link"
-        to="/"
-        class="mt-5"
-        outlined
-        expanded
-        ><b-icon icon="angle-left-b" /> Go back</b-button
-      >
+      <div class="box">
+        <b-upload drag-drop expanded>
+          <section class="section">
+            <div class="content has-text-centered">
+              <p>
+                <b-icon icon="upload" size="is-large"> </b-icon>
+              </p>
+              <p>Drop your certificate file here or click to upload</p>
+            </div>
+          </section>
+        </b-upload>
+        <b-button
+          type="is-primary"
+          tag="router-link"
+          to="/"
+          class="mt-5"
+          inverted
+          expanded
+          ><b-icon icon="angle-left-b" /> Go back</b-button
+        >
+      </div>
     </div>
   </div>
 </template>
