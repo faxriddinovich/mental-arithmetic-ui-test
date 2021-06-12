3637bf04aab7302170bb353b220ecc44bdab8586
mhw0 <mhw0@yahoo.com>
2021-06-12 15:56:34 +0500

    new abacus

--- src/views/FlashCard.vue
+++ src/views/FlashCard.vue
@@ -1,14 +1,14 @@
 <template>
   <div
-    class="box ml-4 mt-4 p-0 is-inline-block is-select-none"
-    style="border: #604021 solid 15px; border-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmhG4CvCzW8GLoWW87i6yVataIC35KB6OdCg&usqp=CAU) 30 round"
+    class="box ml-4 mt-4 p-0 is-inline-block"
+    style="border: #6d5c49 solid 15px; border-radius: 20px" 
   >
     <img
       v-for="index in 6"
       :key="index"
-      width="80"
-      style="margin-bottom: -6px;"
-      src="https://vectr.com/tmp/b1WLVFgiF/a4Detnl0WX.svg?width=51&height=229&select=a4Detnl0WXpage0"
+      width="50"
+      class="abacus_row"
+src="../../public/abacus/skins/yellow/full.svg"
     />
   </div>
 </template>
@@ -20,3 +20,10 @@ import Component from "vue-class-component";
 @Component
 export default class FlashCard extends Vue {}
 </script>
+
+<style>
+.abacus_row {
+  margin-bottom: -7px;
+user-drag: none;
+}
+</style>
