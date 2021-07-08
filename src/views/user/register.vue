8f9bed787d6cd544a4491b70f2771dc257ef9d8b
mhw0 <mhw0@yahoo.com>
2021-07-08 23:05:18 +0500

    fix register

--- src/views/user/register.vue
+++ src/views/user/register.vue
@@ -4,21 +4,21 @@
       <div class="columns is-multiline mx-2">
         <div class="column is-12">
           <div class="box">
-            <b-field label="Login">
-              <b-input v-model="name" icon="user" placeholder="Enter your login"></b-input>
+            <b-field label="Username">
+              <b-input v-model="username" icon="user" placeholder="Enter your username"></b-input>
             </b-field>
             <b-field label="Email">
               <b-input type="email" icon="envelope" placeholder="Enter your email"></b-input>
             </b-field>
             <b-field label="Select your role">
-              <b-radio-button native-value="Base" v-model="role"><b-icon icon="user-square" /> <span>Simple user</span></b-radio-button>
-              <b-radio-button native-value="Teacher" v-model="role"><b-icon icon="graduation-cap" /> <span>Teacher</span></b-radio-button>
+              <b-radio-button native-value="user" v-model="role"><b-icon icon="user-square" /> <span>Simple user</span></b-radio-button>
+              <b-radio-button native-value="teacher" v-model="role"><b-icon icon="graduation-cap" /> <span>Teacher</span></b-radio-button>
             </b-field>
             <b-field label="Password">
-              <b-input type="password" icon="keyhole-circle" placeholder="Enter your password" password-reveal></b-input>
+              <b-input v-model="password" type="password" icon="keyhole-circle" placeholder="Enter your password" password-reveal></b-input>
             </b-field>
             <b-field label="Repeat the password">
-              <b-input type="password" icon="keyhole-circle" placeholder="Repeat the password"></b-input>
+              <b-input v-model="repeatPassword" type="password" icon="keyhole-circle" placeholder="Repeat the password"></b-input>
             </b-field>
             <b-field>
               <b-button type="is-primary" icon-left="plus" @click="showAgreements" expanded>Create an account</b-button>
@@ -38,10 +38,20 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { HTTPClient } from '@theta-rpc/http-client';
 
 @Component
 export default class UserRegister extends Vue {
-  public role: string = "Base";
+  public username!: string;
+  public email!: string;
+  public role: string = "user";
+  public password!: string;
+  public repeatPassword!: string;
+
+  private async login() {
+    const client = new HTTPClient('http://localhost:8000');
+    const result = await client.call('user.create', { });
+  }
 
   public showAgreements() {
     this.$buefy.dialog.confirm({
@@ -58,7 +68,8 @@ export default class UserRegister extends Vue {
       confirmText: 'Confirm',
       hasIcon: true,
       icon: 'exclamation-circle',
-      size: 'is-small'
+      size: 'is-small',
+      onConfirm: () => this.login()
     });
   }
 }
