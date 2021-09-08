792f2b88db5919c1541b7aab3d11a67793cdda84
mhw0 <mhw0@yahoo.com>
2021-09-08 17:25:50 +0500

    feat: filter & search courses

--- src/views/course/courses.vue
+++ src/views/course/courses.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div class="is-flex card p-3">
+    <div class="card p-3">
+    <form @submit.prevent="search" class="is-flex">
       <b-dropdown
         v-model="filter"
         :mobile-modal="false"
@@ -11,27 +12,32 @@
           <b-button icon-left="filter">Filter</b-button>
         </template>
 
-        <b-dropdown-item value="option1" aria-role="listitem">
-          <span>Only free courses</span>
+        <b-dropdown-item value="price" aria-role="listitem">
+        <b-icon icon="dollar-alt" />
+          Price
         </b-dropdown-item>
 
-        <b-dropdown-item value="option2" aria-role="listitem">
-          <span>Last added</span>
+        <b-dropdown-item value="popular" aria-role="listitem">
+          <b-icon icon="star" />
+          Popular
         </b-dropdown-item>
 
-        <b-dropdown-item value="option3" aria-role="listitem">
-          <span>Popular</span>
+        <b-dropdown-item value="purchased" aria-role="listitem">
+          <b-icon icon="shopping-basket" />
+          Purchased
         </b-dropdown-item>
       </b-dropdown>
       <b-input
+        v-model="searchText"
         class="is-flex-grow-1 ml-3"
         placeholder="Course name, author name"
         icon="search"
       >
       </b-input>
-      <b-button type="is-primary" class="ml-3" icon-left="search"
+      <b-button native-type="submit" type="is-primary" class="ml-3" icon-left="search"
         >Search</b-button
       >
+      </form>
     </div>
 
     <div class="mt-5">
@@ -40,7 +46,7 @@
         text="No courses found"
         v-if="!isLoading && !courses.length"
       />
-      <div v-if="courses.length">
+      <div v-if="!isLoading && courses.length">
         <div class="columns is-mobile is-multiline">
           <div
             class="column is-12-mobile is-6-tablet is-4-desktop"
@@ -72,14 +78,25 @@ import NotFoundBox from "@/components/not-found-box.vue";
 })
 export default class Courses extends Vue {
   public filter = [];
-  public isLoading = true;
+  public searchText = '';
+  public isLoading = false;
   public courses = [];
 
   mounted() {
-    setTimeout(async () => {
-      this.courses = await rpc.call("get_courses");
-      this.isLoading = false;
-    }, 1000);
+    this.isLoading = true;
+    rpc.call('get_courses').then((courses) => {
+      this.courses = courses;
+    }).finally(() => this.isLoading = false);
+  }
+
+  public search() { 
+    const params = {};
+    if(this.filter.length) params['filter'] = this.filter;
+    if(this.searchText.length) params['searchText'] = this.searchText;
+    this.isLoading = true;
+    rpc.call('get_courses', params).then((courses) => {
+      this.courses = courses;
+    }).finally(() =>  this.isLoading = false );
   }
 }
 </script>
