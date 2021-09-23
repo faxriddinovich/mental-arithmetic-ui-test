912a0dcd5fa0253a79f9031f1b280dd660352194
mhw0 <mhw0@yahoo.com>
2021-09-23 12:42:24 +0500

    scores

--- src/components/charts/line.chart.vue
+++ src/components/charts/line.chart.vue
@@ -1,14 +1,14 @@
 <script lang="ts">
-import { Component, Prop, Mixins } from 'vue-property-decorator';
+import { Component, Prop, Mixins } from "vue-property-decorator";
 import { Line } from "vue-chartjs";
 
 @Component
 export default class LineChart extends Mixins(Line) {
-  @Prop(Object) public chartdata!;
-  @Prop(Object) public options!;
+  @Prop(Object) public chartdata;
+  @Prop(Object) public options;
 
   mounted() {
     this.renderChart(this.chartdata, this.options);
-	}
+  }
 }
 </script>
