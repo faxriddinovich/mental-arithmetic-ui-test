2d11d1a6a1242a2bedb891a390c7e3f513a0956d
mhw0 <mhw0@yahoo.com>
2021-10-28 12:42:01 +0500

    feat: we're using electron

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,18 @@
 module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+    }
+  },
+
   devServer: {
     disableHostCheck: true,
   },
+
   chainWebpack: (config) => {
     config.resolve.symlinks(false);
     if (process.env.NODE_ENV === "development") {
       config.output.filename("[name].[hash].js").end();
     }
-  },
+  }
 };
