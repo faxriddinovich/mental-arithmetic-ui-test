c21492ad9c2a2adcb4f50cb2719f90cf53bdbf27
mhw0 <mhw0@yahoo.com>
2021-10-29 09:26:10 +0500

    feat: preprocessor-loader is awesome

--- vue.config.js
+++ vue.config.js
@@ -4,15 +4,28 @@ module.exports = {
       nodeIntegration: true,
     },
   },
-
   devServer: {
     disableHostCheck: true,
   },
-
   chainWebpack: (config) => {
     config.resolve.symlinks(false);
     if (process.env.NODE_ENV === "development") {
       config.output.filename("[name].[hash].js").end();
     }
+
+    config.module
+      .rule("preprocessor")
+      .test(/\.vue$/)
+      .use("webpack-preprocessor-loader")
+      .loader("webpack-preprocessor-loader")
+      .options({
+        params: {
+          __DESKTOP__: !!process.env.DESKTOP,
+          __MOBILE__: !!process.env.MOBILE,
+          __BROWSER__: !!process.env.BROWSER,
+        },
+        verbose: false,
+      })
+      .end();
   },
 };
