352cea546e29423fbab00d998c769642e7ff100a
mhw0 <mhw0@yahoo.com>
2021-11-19 08:39:08 +0500

    WIP(ui): add some new things

--- src/views/games/numbers/numbers.vue
+++ src/views/games/numbers/numbers.vue
@@ -6,226 +6,214 @@
     <div class="column is-5-widescreen is-6-desktop is-12-mobile is-9-tablet">
       <div class="box">
         <form @submit.prevent="play">
-          <b-field>
-            <template #label> <b-icon icon="file" /> Theme </template>
-            <b-autocomplete
-              rounded
-              placeholder="Search a theme"
-              icon="search"
-              clearable
+          <template v-if="currentTab === 0">
+            <b-field>
+              <template #label> <b-icon icon="file" /> Theme </template>
+              <p class="control">
+                <b-dropdown>
+                  <template #trigger>
+                    <b-button label="Digits" icon-right="dialpad-alt" />
+                  </template>
+
+                  <b-dropdown-item custom>Select Digits</b-dropdown-item>
+                  <b-dropdown-item separator></b-dropdown-item>
+                  <b-dropdown-item value="1">1</b-dropdown-item>
+                  <b-dropdown-item value="2">2</b-dropdown-item>
+                  <b-dropdown-item value="3">3</b-dropdown-item>
+                  <b-dropdown-item value="4">4</b-dropdown-item>
+                  <b-dropdown-item value="5">5</b-dropdown-item>
+                  <b-dropdown-item value="6">6</b-dropdown-item>
+                </b-dropdown>
+              </p>
+              <b-autocomplete
+                rounded
+                placeholder="Search a theme"
+                icon="search"
+                clearable
+                expanded
+              >
+                <template #empty>No results found</template>
+              </b-autocomplete>
+              <p class="control">
+                <b-button type="is-success" label="Add" icon-left="plus" />
+              </p>
+            </b-field>
+
+            <b-button
+              type="is-primary mt-5"
+              icon-left="arrow-right"
+              @click="currentTab = 1"
               expanded
+              >Next</b-button
             >
-              <template #empty>No results found</template>
-            </b-autocomplete>
-            <b-dropdown v-model="theme" aria-role="list" class="ml-4">
-              <template #trigger>
-                <b-button
-                  label="Digits"
-                  type="is-primary"
-                  icon-left="angle-down"
-                />
-              </template>
-
-              <b-dropdown-item value="1" aria-role="listitem">
-                <div class="media">
-                  <b-icon class="media-left" icon="angle-right"></b-icon>
-                  <div class="media-content">1</div>
-                </div>
-              </b-dropdown-item>
-              <b-dropdown-item value="2" aria-role="listitem">
-                <div class="media">
-                  <b-icon class="media-left" icon="angle-right"></b-icon>
-                  <div class="media-content">2</div>
-                </div>
-              </b-dropdown-item>
-            </b-dropdown>
-          </b-field>
-          <div class="columns is-mobile">
-            <div class="column">
-              <b-field>
-                <template #label>
-                  <b-icon icon="angle-right" /> Examples:
-                </template>
-                <b-numberinput
-                  v-model="periodicity"
-                  :controls="false"
-                ></b-numberinput>
-              </b-field>
-            </div>
-            <div class="column">
-              <b-field>
-                <template #label>
-                  <b-icon icon="angle-right" /> Rows:
-                </template>
-                <b-numberinput
-                  v-model="periodicity"
-                  :controls="false"
-                ></b-numberinput>
-              </b-field>
-            </div>
-            <!--
-            <div class="column">
-            <b-field>
-              <template #label>
-                <b-icon icon="angle-right" /> Direction:
-              </template>
-            <b-radio-button v-model="direction"
-                native-value="right"
-                type="is-primary is-light is-outlined">
-                <b-icon icon="arrow-right" class="m-0"></b-icon>
-            </b-radio-button>
-
-<b-radio-button v-model="direction"
-                native-value="top"
-                type="is-danger is-light is-outlined"> <b-icon icon="arrow-down" class="m-0"></b-icon> </b-radio-button>
-
-            <b-radio-button v-model="transformation"
-                native-value="Yep"
-                type="is-success is-light is-outlined">
-                <b-icon icon="arrow-up" class="m-0"></b-icon>
-            </b-radio-button>
-
-
-            </b-field>
-            </div>
--->
-          </div>
-
-          <div class="columns is-mobile">
-            <div class="column">
-              <b-field>
-                <template #label>
-                  <b-icon icon="clock" /> Delay between examples:
-                </template>
-                <b-numberinput
-                  v-model="periodicity"
-                  :controls="false"
-                ></b-numberinput>
-              </b-field>
-            </div>
-            <div class="column">
-              <b-field>
-                <template #label>
-                  <b-icon icon="clock" /> Delay between rows:
-                </template>
-                <b-numberinput
-                  v-model="periodicity"
-                  :controls="false"
-                ></b-numberinput>
-              </b-field>
-            </div>
-          </div>
-
-          <b-collapse
-            :open="false"
-            position="is-bottom"
-            aria-id="contentIdForA11y1"
-          >
-            <template #trigger="props">
-              <a aria-controls="contentIdForA11y1">
-                <span v-if="!props.open">
-                  <b-icon icon="cog"></b-icon>
-                  Advenced options
-                </span>
-              </a>
-            </template>
-            <b-field>
-              <template #label>
-                <b-icon icon="palette" /> Numbers Color:
-              </template>
-              <div class="is-flex">
-                <span
-                  class="is-color is-selected"
-                  style="background: #34495e"
-                />
-                <span
-                  class="ml-2 is-color is-large"
-                  style="background: #1abc9c"
-                />
-                <span
-                  class="ml-2 is-color is-large"
-                  style="background: #27ae60"
-                />
-                <span
-                  class="ml-2 is-color is-large"
-                  style="background: #2ecc71"
-                />
-                <span
-                  class="ml-2 is-color is-large"
-                  style="background: #3498db"
-                />
-                <span
-                  class="ml-2 is-color is-large"
-                  style="background: #9b59b6"
-                />
-                <span
-                  class="ml-2 is-color is-large"
-                  style="background: #f1c40f"
-                />
-                <span
-                  class="ml-2 is-color is-large"
-                  style="background: #e67e22"
-                />
-                <span
-                  class="ml-2 is-color is-large"
-                  style="background: #e74c3c"
-                />
+          </template>
+          <template v-else>
+            <div class="columns is-mobile">
+              <div class="column">
+                <b-field>
+                  <template #label>
+                    <b-icon icon="angle-right" /> Examples:
+                  </template>
+                  <b-numberinput
+                    v-model="periodicity"
+                    :controls="false"
+                  ></b-numberinput>
+                </b-field>
               </div>
-            </b-field>
+              <div class="column">
+                <b-field>
+                  <template #label>
+                    <b-icon icon="angle-right" /> Rows:
+                  </template>
+                  <b-numberinput
+                    v-model="periodicity"
+                    :controls="false"
+                  ></b-numberinput>
+                </b-field>
+              </div>
+            </div>
 
-            <b-field>
-              <template #label>
-                <b-icon icon="crop-alt-rotate-right" /> Numbers Transformation
+            <div class="columns is-mobile">
+              <div class="column">
+                <b-field>
+                  <template #label>
+                    <b-icon icon="clock" /> Delay between examples:
+                  </template>
+                  <b-numberinput
+                    v-model="periodicity"
+                    :controls="false"
+                  ></b-numberinput>
+                </b-field>
+              </div>
+              <div class="column">
+                <b-field>
+                  <template #label>
+                    <b-icon icon="clock" /> Delay between rows:
+                  </template>
+                  <b-numberinput
+                    v-model="periodicity"
+                    :controls="false"
+                  ></b-numberinput>
+                </b-field>
+              </div>
+            </div>
+
+            <b-collapse
+              :open="false"
+              position="is-bottom"
+              aria-id="contentIdForA11y1"
+            >
+              <template #trigger="props">
+                <a aria-controls="contentIdForA11y1">
+                  <span v-if="!props.open">
+                    <b-icon icon="cog"></b-icon>
+                    Advenced options
+                  </span>
+                </a>
               </template>
+              <b-field>
+                <template #label>
+                  <b-icon icon="palette" /> Numbers Color:
+                </template>
+                <div class="is-flex">
+                  <span
+                    class="is-color is-selected"
+                    style="background: #34495e"
+                  />
+                  <span
+                    class="ml-2 is-color is-large"
+                    style="background: #1abc9c"
+                  />
+                  <span
+                    class="ml-2 is-color is-large"
+                    style="background: #27ae60"
+                  />
+                  <span
+                    class="ml-2 is-color is-large"
+                    style="background: #2ecc71"
+                  />
+                  <span
+                    class="ml-2 is-color is-large"
+                    style="background: #3498db"
+                  />
+                  <span
+                    class="ml-2 is-color is-large"
+                    style="background: #9b59b6"
+                  />
+                  <span
+                    class="ml-2 is-color is-large"
+                    style="background: #f1c40f"
+                  />
+                  <span
+                    class="ml-2 is-color is-large"
+                    style="background: #e67e22"
+                  />
+                  <span
+                    class="ml-2 is-color is-large"
+                    style="background: #e74c3c"
+                  />
+                </div>
+              </b-field>
 
-              <b-radio-button
-                v-model="transformation"
-                native-value="0"
-                type="is-primary"
+              <b-field>
+                <template #label>
+                  <b-icon icon="crop-alt-rotate-right" /> Numbers Transformation
+                </template>
+
+                <b-radio-button
+                  v-model="transformation"
+                  native-value="0"
+                  type="is-primary"
+                >
+                  <b-icon icon="rotate-360"></b-icon>
+                  <span style="-webkit-transform: rotate(0deg)">-123</span>
+                </b-radio-button>
+
+                <b-radio-button
+                  v-model="transformation"
+                  native-value="90"
+                  type="is-primary"
+                >
+                  <b-icon icon="rotate-360"></b-icon>
+                  <span style="-webkit-transform: rotate(90deg)">-123</span>
+                </b-radio-button>
+
+                <b-radio-button
+                  v-model="transformation"
+                  native-value="180"
+                  type="is-primary"
+                >
+                  <b-icon icon="rotate-360"></b-icon>
+                  <span style="-webkit-transform: rotate(180deg)">-123</span>
+                </b-radio-button>
+
+                <b-radio-button v-model="transformation" native-value="270">
+                  <b-icon icon="rotate-360"></b-icon>
+                  <span style="-webkit-transform: rotate(270deg)">-123</span>
+                </b-radio-button>
+              </b-field>
+              <b-field>
+                <template #label>
+                  <b-icon icon="volume" /> Display & Sound
+                </template>
+                <b-checkbox>Display numbers</b-checkbox>
+                <b-checkbox>Has Sound</b-checkbox>
+              </b-field>
+            </b-collapse>
+            <div class="is-flex mt-5">
+              <b-button icon-left="arrow-left" @click="currentTab = 0"
+                >Back</b-button
               >
-                <b-icon icon="rotate-360"></b-icon>
-                <span style="-webkit-transform: rotate(0deg)">-123</span>
-              </b-radio-button>
-
-              <b-radio-button
-                v-model="transformation"
-                native-value="90"
+              <b-button
                 type="is-primary"
+                class="ml-2"
+                icon-left="play"
+                @click="currentTab = 1"
+                expanded
+                >Play</b-button
               >
-                <b-icon icon="rotate-360"></b-icon>
-                <span style="-webkit-transform: rotate(90deg)">-123</span>
-              </b-radio-button>
-
-              <b-radio-button
-                v-model="transformation"
-                native-value="180"
-                type="is-primary"
-              >
-                <b-icon icon="rotate-360"></b-icon>
-                <span style="-webkit-transform: rotate(180deg)">-123</span>
-              </b-radio-button>
-
-              <b-radio-button v-model="transformation" native-value="270">
-                <b-icon icon="rotate-360"></b-icon>
-                <span style="-webkit-transform: rotate(270deg)">-123</span>
-              </b-radio-button>
-            </b-field>
-            <b-field>
-              <template #label>
-                <b-icon icon="volume" /> Display & Sound
-              </template>
-              <b-checkbox>Display numbers</b-checkbox>
-              <b-checkbox>Has Sound</b-checkbox>
-            </b-field>
-          </b-collapse>
-
-          <b-button
-            native-type="submit"
-            type="is-primary mt-5"
-            icon-left="play"
-            expanded
-            >Play</b-button
-          >
+            </div>
+          </template>
         </form>
       </div>
       <div class="mx-2">
@@ -244,6 +232,8 @@
 import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class GameNumbers extends Vue {
+  public currentTab = 0;
+
   public digits = "1";
   public periodicity = 4;
   public transformation = "0";
