870989ac50ce2f2dfb1964b2f7d1580a0cf7d0b5
mhw0 <mhw0@yahoo.com>
2021-12-03 15:03:50 +0500

    WIP: the numbers game is better now

--- src/views/games/numbers/play.ts
+++ src/views/games/numbers/play.ts
@@ -3,11 +3,14 @@ import {
   computed,
   onMounted,
   ref,
-  reactive
 } from "@vue/composition-api";
-import {QueueItem} from "./interfaces";
 import {createNamespacedHelpers as createStoreHelper} from "vuex-composition-helpers";
-// FIXME: this is the worst idea to keep the interfaces in the views folder. So this must be fixed in the future
+import {NotificationProgrammatic as Notification} from 'buefy'
+
+/*
+ * FIXME: this is the worst idea to keep the interfaces in the
+ * views folder. So this must be fixed in the future
+ */
 import {NumbersGameSettings} from '@/views/games/numbers/interfaces';
 import {showToastMessage, ToastType} from '@/services/toast';
 
@@ -20,11 +23,11 @@ export default defineComponent({
       createStoreHelper<{settings: NumbersGameSettings}>("GameModule");
     const {settings} = useState(["settings"]);
 
-    //these are not reactive
     const queue = settings.value.queue
-    const currentQueueItemIndex = 0;
+    let currentQueueItemIndex = 0;
     let currentExampleIndex = 0;
 
+    const answerAtEnd = ref<boolean>(settings.value.answerAtEnd);
     const canEnterAnswer = ref<boolean>(false);
     const centerText = ref<string | null>();
     const answer = ref<number | null>();
@@ -43,75 +46,85 @@ export default defineComponent({
       return {color: currentQueueItem.fontColor};
     });
 
+    const clearCenterText = () => centerText.value = null;
+    const setCenterText = (text: any) => centerText.value = text;
+    const showAnswerForm = () => {answer.value = null; canEnterAnswer.value = true};
+    const hideAnswerForm = () => canEnterAnswer.value = false;
+
+    /*
+     * Resolves the promise after showing all the rows
+     */
+    function renderExampleRows(rows: (string | number)[], interval: number): Promise<void> {
+      let currentRowIndex = 0;
+      return new Promise((resolve) => {
+        const rowsInterval = setInterval(() => {
+          if (rows[currentRowIndex]) {
+            setCenterText(rows[currentRowIndex]);
+            currentRowIndex++;
+          } else {
+            clearInterval(rowsInterval); // remove the handle
+            currentRowIndex = 0; // free
+            resolve();
+          }
+        }, interval);
+      });
+    }
 
     function showExamples() {
-      let currentExampleRowIndex = 0;
       const currentQueueItem = queue[currentQueueItemIndex];
+      const currentExample = currentQueueItem.examples[currentExampleIndex];
 
-      const exampleRowInterval = setInterval(() => {
-        const currentExample = currentQueueItem.examples[currentExampleIndex];
-
-        // if there is a row
-        if (currentExample.numbers[currentExampleRowIndex]) {
-          centerText.value = currentExample.numbers[currentExampleRowIndex];
-          currentExampleRowIndex++;
-        } else {
-          clearInterval(exampleRowInterval);
-          centerText.value = null;
-          // currentExampleIndex actually is the next 
-          if (currentQueueItem.examples[currentExampleIndex]) {
-            canEnterAnswer.value = true;
+      /* if the current example exists */
+      if (currentExample) {
+        /* show all the rows of the current example */
+        renderExampleRows(currentExample.numbers, 500 /* FIXME */).then(() => {
+          /* clear after showing all the rows */
+          clearCenterText();
+          currentExampleIndex++;
+          /* if `answerAtEnd` is true, then we should show the answer form */
+          if (!settings.value.answerAtEnd) {
+            showAnswerForm();
+          } else { /* otherwise after 2 seconds we show the rows of the next example */
+            setTimeout(() => {showExamples()}, 2000);
           }
-        }
-        // FIXME: fix the interval time
-      }, 500);
+        });
+      } else if (queue[currentQueueItemIndex + 1]) { /* if there are items in the queue */
+        /* go to the next queue item */
+        currentQueueItemIndex++;
+        /* clear the index */
+        currentExampleIndex = 0;
+        /* show the name of the next item in the queue */
+        setCenterText(queue[currentQueueItemIndex].theme);
+        /* after 2 seconds show the examples of the next item in the queue */
+        setTimeout(() => {
+          showExamples();
+        }, 2000);
+      } else { /* there are no queue items left */
+        if (settings.value.answerAtEnd)
+          showAnswerForm();
+      }
     }
 
     function enterAnswer() {
       const currentQueueItem = queue[currentQueueItemIndex];
-      const currentExample = currentQueueItem.examples[currentExampleIndex];
-
-      if (currentExample.answer == answer.value) {
-        showToastMessage('Correct!', ToastType.Success);
-      } else {
-        showToastMessage('Incorrect! The correct answer is: ' + currentExample.answer, ToastType.Danger);
-      }
-
-      canEnterAnswer.value = false;
-      answer.value = null;
-
-      if (currentQueueItem.examples[currentExampleIndex + 1]) {
-        currentExampleIndex++;
-        showExamples();
-      } else {
-        centerText.value = 'End!';
-      }
-
-    }
-
-    /*
-     * Immediately shows the "start texts"
-     */
-    function showStartAttentionTexts() {
-      let currentStartAttention = 1;
-      centerText.value = START_ATTENTION_TEXTS[0];
-
-      const startAttentionInterval = setInterval(() => {
-        if (START_ATTENTION_TEXTS[currentStartAttention]) {
-          centerText.value = START_ATTENTION_TEXTS[currentStartAttention];
-          currentStartAttention++;
-          return;
+      const prevExample = currentQueueItem.examples[currentExampleIndex - 1];
+      if (prevExample.answer == answer.value) {
+        if (currentExampleIndex % 3 === 0) {
+          Notification.open({type: 'is-success', message: 'Keep going!', hasIcon: true, position: 'is-bottom'});
         }
 
-        clearInterval(startAttentionInterval);
-        // after showing the start texts we can start the game
-        showExamples();
-      }, 1000);
+        showToastMessage("Correct!", ToastType.Success);
+      } else {
+        showToastMessage("Incorrect! The correct answer was: <b>" + prevExample.answer + "</b>", ToastType.Danger);
+      }
+
+      hideAnswerForm();
+      showExamples();
     }
 
     onMounted(() => {
-      // entry
-      showStartAttentionTexts();
+      /* FIXME: renderExampleRows is not funny */
+      renderExampleRows(START_ATTENTION_TEXTS, 800).then(() => showExamples());
     });
 
     return {
@@ -120,7 +133,8 @@ export default defineComponent({
       fontClasses,
       fontStyles,
       centerText,
-      canEnterAnswer
+      canEnterAnswer,
+      answerAtEnd
     };
   },
 });
