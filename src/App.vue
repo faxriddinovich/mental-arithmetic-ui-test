bd571757e2d7d19828cf23a9e9d19b997f403be2
mhw0 <mhw0@yahoo.com>
2022-01-04 21:28:35 +0500

    add splash screen

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,53 @@
 <template>
-  <div class="is-unselectable" ref="container">
-    <router-view :key="$route.fullPath" />
+  <div class="is-unselectable">
+    <section class="hero is-fullheight" v-if="isLoading">
+      <div class="hero-body is-flex-direction-column is-justify-content-center">
+        <img :src="require('@@/img/dual-rings-loading.svg')" width="100px" />
+        <div class="is-size-4">{{ loadingText }}</div>
+      </div>
+    </section>
+    <div v-else>
+      <router-view :key="$route.fullPath" />
+    </div>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "@vue/composition-api";
+
+export default defineComponent({
+  setup(_, context) {
+    const loadingText = ref<string>("");
+    const isLoading = ref<boolean>(true);
+    context.root.$store.dispatch("TextToSpeech/updateLanguage", "ru");
+
+    // some magic
+    const sleep = async (ms: number) =>
+      new Promise((resolve) => setTimeout(resolve, ms));
+
+    onMounted(async () => {
+      loadingText.value = "Syncing settings..";
+      await context.root.$store.dispatch("Settings/sync");
+      await sleep(500);
+
+      const locale = context.root.$store.getters["Settings/get"]("locale");
+
+      loadingText.value = "Initializing locale..";
+      await sleep(500);
+      context.root.$i18n.locale = locale;
+
+      loadingText.value = "Initializing TTS service..";
+      await sleep(500);
+      await context.root.$store.dispatch("TextToSpeech/updateLanguage", locale);
+
+
+      isLoading.value = false;
+    });
+
+    return { isLoading, loadingText };
+  },
+});
+</script>
+
 <style lang="scss">
 @import "https://unicons.iconscout.com/release/v2.1.11/css/unicons.css";
 @import "https://unicons.iconscout.com/release/v4.0.0/css/solid.css";
