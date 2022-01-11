c0a4c9cbc1c9ae06f16abdcef85aaa8b971db159
mhw0 <mhw0@yahoo.com>
2022-01-11 23:34:55 +0500

    some cool things

--- src/views/games/abacus/game.vue
+++ src/views/games/abacus/game.vue
@@ -1,34 +1,67 @@
 <template>
-  <div ref="abacusContainerRef" style="height: 100vh"></div>
+  <div ref="abacusContainerRef">
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref, onMounted } from "@vue/composition-api";
-import { SVG } from "@svgdotjs/svg.js";
-import {
-  AbacusBoard,
-} from "./board";
+import { SVG, Text } from "@svgdotjs/svg.js";
+import { AbacusBoard } from "./board";
 
 export default defineComponent({
   setup() {
     const abacusContainerRef = ref<HTMLElement>();
+    const numbersContainerRef = ref<HTMLElement>();
     const abacusValue = ref<number>(0);
 
     onMounted(() => {
-      const draw = SVG().addTo(abacusContainerRef.value!).size("100%", "100%");
-      const abacusBoard = new AbacusBoard(3);
+      const draw = SVG().addTo(abacusContainerRef.value!).size("100%", "100%").css('position', 'absolute');
+      const foreignObject = draw.foreignObject(1000, 200);
+      const numberDraw = SVG().addTo(foreignObject).viewbox(0, 0, 1000, 140);
+
+      foreignObject.cx(window.innerWidth / 2);
+      foreignObject.cy(200);
+
+      const abacusBoard = new AbacusBoard(6);
       abacusBoard.draw();
       draw.add(abacusBoard);
       abacusBoard.construct();
 
-      abacusBoard.on('update', (value) => {
+      abacusBoard.on("update", (value) => {
         abacusValue.value = (value as CustomEvent<number>).detail;
       });
 
       abacusBoard.cx(window.innerWidth / 2);
-      abacusBoard.cy(window.innerHeight / 2);
+      abacusBoard.cy(700);
+
+      const numbers = ['+44', '+91', '-44', '+15', '+44', '+91', '-44', '+15', '+44', '+91', '-44', '+15', '+44', '+91', '-44', '+15'];
+
+      const tt = numberDraw.text((text) => {
+        for(const n of numbers)
+          text.tspan(n);
+      }).font({ size: '10em', weight: "bold" }).fill("rgba(0, 0, 0, .2)");
+      tt.attr({ 'dominant-baseline': "hanging" });
+
+      tt.x(1000);
+
+      let currIndex = 0;
+      const timerHandle = setInterval(() => {
+        const childs = tt.children();
+        if (childs[currIndex]) {
+          if (childs[currIndex - 1])
+            childs[currIndex - 1].fill("rgba(0, 0, 0, .2)");
+
+          childs[currIndex].fill("rgba(0, 0, 0, .7)");
+          //numberDraw.animate(100).viewbox(childs[currIndex].cx() - 500, 0, 1000, 200);
+          tt.animate(200).dx(-childs[currIndex].cx() + (500 - childs[currIndex].width()));
+          currIndex++;
+          return;
+        }
+
+        clearInterval(timerHandle);
+      }, 1000);
     });
 
-    return { abacusContainerRef, abacusValue };
+    return { numbersContainerRef, abacusContainerRef, abacusValue };
   },
 });
 </script>
