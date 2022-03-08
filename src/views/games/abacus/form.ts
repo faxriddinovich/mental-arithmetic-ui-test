cb82aed3fea24c75d1373205393c77d13a5813d4
mhw0 <mhw0@yahoo.com>
2022-03-08 13:36:11 +0500

    feat: automatically select abacus columns count

--- src/views/games/abacus/form.ts
+++ src/views/games/abacus/form.ts
@@ -1,4 +1,4 @@
-import { defineComponent, computed, ref } from "@vue/composition-api";
+import { defineComponent, computed, ref, watch } from "@vue/composition-api";
 import { SequenceItem } from "@/views/games/abacus/interfaces";
 import ColorPalette from "@/components/games/color-palette.vue";
 import ThemesInputField from "@/components/games/themes-input-field.vue";
@@ -30,6 +30,10 @@ export default defineComponent({
     const abacusColumnsCount = ref<number>(10);
 
     const sequence = ref<SequenceItem[]>([]);
+
+    watch(digit, (newDigit) => {
+      abacusColumnsCount.value = newDigit;
+    });
 
     const canAddSequenceItem = computed(() => {
       return (
